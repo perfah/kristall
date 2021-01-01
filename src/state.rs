7c4a2b233dbc1f01e063ca311086372c00410c6c
Per Fahlander <perfah@kth.se>
2021-01-01 14:00:55 +0100

    Refactor camera system

--- src/state.rs
+++ src/state.rs
@@ -8,8 +8,9 @@ use crate::backend::graphics::model::Model;
 use winit::window::Window;
 use crate::world::entity::EntityContainer;
 use crate::world::entity::component::model::GraphicsModel;
-use winit::event::WindowEvent;
-use crate::world::entity::component::camera::Camera;
+use winit::event::DeviceEvent;
+use crate::backend::graphics::camera::Camera;
+use crate::world::entity::component::camera::Camera as CameraComponent;
 use rand_core::SeedableRng;
 use crate::world::entity::prefab::rand_tile::RandomTile;
 use cgmath::Vector3;
@@ -17,7 +18,7 @@ use cgmath::Vector3;
 pub struct State {
     graphics_backend: WGPUState,
     world: World,
-    camera: ComponentManager<Camera>,
+    camera: Camera,
     instances: Vec<(ComponentManager<Transform>, &'static str)>,
     loaded_models: HashMap<&'static str, Model>,
     delta: Duration,
@@ -28,11 +29,19 @@ impl State {
     pub async fn new(window: &Window, vsync: bool) -> Self {
         let world_generator = RandomTile::from_entropy();
         let world = World::new(world_generator);
+        let (camera_component, target) = world.query_entities(true)
+            .map(|entity| (entity.component::<CameraComponent>(), entity.component::<Transform>()))
+            .filter(|(a,b)| a.is_some() && b.is_some())
+            .map(|(a,b)| (a.unwrap(), b.unwrap()))
+            .next()
+            .unwrap();
+
+        let camera = Camera::new(camera_component, target);
 
         Self {
             graphics_backend: WGPUState::new(window, vsync).await,
             world: world.clone(),
-            camera: world.query_components(true).next().unwrap(),
+            camera,
             instances: Vec::new(),
             loaded_models: HashMap::new(),
             delta: Duration::new(0, 0),
@@ -69,13 +78,13 @@ impl State {
         }
     }
 
-    pub fn resize(&mut self, new_size: winit::dpi::PhysicalSize<u32>){
-        self.camera.peek_mut(|camera| camera.aspect = new_size.width as f32 / new_size.height as f32);
+    pub fn resize(&mut self, new_size: winit::dpi::PhysicalSize<u32>, window: &Window){
         self.graphics_backend.resize(new_size);
+        self.camera.resize(new_size, window);
     }
 
-    pub fn input(&mut self, event: &WindowEvent) -> bool {
-        self.camera.peek_mut(|camera| camera.process_events(event)).unwrap()
+    pub fn input(&mut self, event: &DeviceEvent, window: &Window) -> bool {
+        self.camera.process_events(event, &window)
     }
 
     pub fn update(&mut self) {
@@ -83,34 +92,29 @@ impl State {
         self.delta = now - self.prev_instant;
         self.prev_instant = now;
 
-        let build_proj_matrix = {
-            let mut camera = self.camera.lock_component_for_write();
-            camera.update();
-            camera.build_view_projection_matrix()
-        };
-
-        // TODO
+        let build_proj_matrix = self.camera.view_proj_matrix();
         
-        /*
         let mut play: ComponentManager<Transform> = self.world.query_entity_by_name("player", true).next().unwrap().query_components(true).next().unwrap();
         play.peek_mut(|transform| transform.acc = Vector3{
-            x: 1.0 * (rand::random::<f32>() - 0.2),
+            x: 2.0 * (rand::random::<f32>() - 0.5),
             y: 0.0,
-            z: 1.0 * (rand::random::<f32>() - 0.2)
+            z: 2.0 * (rand::random::<f32>() - 0.5)
         });
-*/
+
 
         self.update_graphics_data();
 
-        self.graphics_backend.update(&self.instances, build_proj_matrix)
-    
-
+        self.graphics_backend.update(&self.instances, build_proj_matrix);
+        self.camera.update(self.delta);
     }
 
     pub fn render(&mut self){
-        let fps =  1000 / self.delta.as_millis();
+        let fps =  1000 / (self.delta.as_millis() + 1u128);
 
         self.graphics_backend.render(&self.instances, &self.loaded_models, fps)
     }
 
+    pub fn set_escape_status(&mut self, window: &Window, escape_status: bool) {
+        self.camera.set_escape_status(window, escape_status)
+    }
 }
--- src/world/entity/component/camera.rs
+++ src/world/entity/component/camera.rs
@@ -5,191 +5,20 @@ use cgmath::{Point3, Vector3, EuclideanSpace, InnerSpace};
 use crate::world::entity::component::{Component, ComponentManager};
 use crate::world::entity::component::transform::Transform;
 
-pub enum CameraPerspective {
-    FirstPersonView,
-    ThirdPersonView{
-        target: ComponentManager<Transform>,
-        distance: f32,
-        angle_xz: f32,
-        angle_xy: f32
-    }
-}
+pub use crate::backend::graphics::camera::CameraPerspective;
+pub use crate::backend::input::camera::CameraController;
 
 pub struct Camera {
     pub perspective: CameraPerspective,
-    pub controller: CameraController,
-    //pub eye: cgmath::Point3<f32>,
-    pub up: cgmath::Vector3<f32>,
-    pub aspect: f32,
-    pub fovy: f32,
-    pub znear: f32,
-    pub zfar: f32,
+    pub controller: Box<dyn CameraController>
 }
 
 impl Camera {
-    pub fn new_third_person(target: ComponentManager<Transform>) -> Camera {
-        Camera {
-            perspective: CameraPerspective::ThirdPersonView{
-                target,
-                distance: 25f32,
-                angle_xz: 0f32,
-                angle_xy: 0f32
-            },
-            controller: CameraController::new(0.2),
-            //eye: (0.0, 5.0, -10.0).into(),
-            up: cgmath::Vector3::unit_y(),
-            aspect: 10 as f32 / 10 as f32,
-            fovy: 45.0,
-            znear: 0.1,
-            zfar: 100.0,
-        }
-    }
-
-    pub fn build_view_projection_matrix(&self) -> cgmath::Matrix4<f32> {
-        match &self.perspective {
-            CameraPerspective::ThirdPersonView { target, distance, angle_xz, angle_xy }=> {
-                let target = target.peek(|transform| Point3::from_vec(transform.pos)).unwrap();
-
-                let tot = 2.0 * std::f32::consts::PI;
-
-                let eye = target - (Vector3::new(
-                    f32::cos(angle_xz % tot) * f32::cos(angle_xy % tot),
-                    f32::sin(angle_xy % tot),
-                    f32::sin(angle_xz % tot) * f32::cos(angle_xy % tot)
-                ).normalize() * (*distance));
-
-                let view = cgmath::Matrix4::look_at(
-                    eye, 
-                    target, 
-                    if f32::cos(angle_xy % tot) > 0f32 { self.up } else { -self.up});
-                let proj = cgmath::perspective(cgmath::Deg(self.fovy), self.aspect, self.znear, self.zfar);
-                return OPENGL_TO_WGPU_MATRIX * proj * view;
-            },
-            _ => unimplemented!()
-        }
-    }
-
-    pub fn update(&mut self) {
-        match self.perspective {
-            CameraPerspective::ThirdPersonView {ref target, ref mut distance, ref mut angle_xz, ref mut angle_xy} => {
-                let target = target.peek(|transform| Point3::from_vec(transform.pos)).unwrap();
-
-                //let forward = target - self.eye;
-                //let forward_norm = forward.normalize();
-                //let forward_mag = forward.magnitude();
-
-                // Prevents glitching when camera gets too close to the
-                // center of the scene.
-                if self.controller.is_forward_pressed { //&& forward_mag > self.controller.speed
-                    //self.eye += forward_norm * self.controller.speed;
-                    *angle_xy -= 0.05f32;
-                }
-                if self.controller.is_backward_pressed {
-                    //self.eye -= forward_norm * self.controller.speed;
-                    *angle_xy += 0.05f32;
-                }
-
-                //let right = forward_norm.cross(self.up);
-
-                // Redo radius calc in case the up/ down is pressed.
-                //let forward = target - self.eye;
-                //let forward_mag = forward.magnitude();
-
-                if self.controller.is_right_pressed {
-                    // Rescale the distance between the target and eye so
-                    // that it doesn't change. The eye therefore still
-                    // lies on the circle made by the target and eye.
-                    //self.eye = target - (forward + right * self.controller.speed).normalize() * forward_mag;
-                    *angle_xz += 0.05f32;
-                }
-                if self.controller.is_left_pressed {
-                    //self.eye = target - (forward - right * self.controller.speed).normalize() * forward_mag;
-                    *angle_xz -= 0.05f32;
-                }
-
-                if self.controller.is_up_pressed {
-                    //self.eye += cgmath::Vector3::unit_y() * 0.5;
-                    *distance += 0.05f32;
-                }
-
-                if self.controller.is_down_pressed {
-                    //self.eye -= cgmath::Vector3::unit_y() * 0.5;
-                    *distance -= 0.05f32;
-                }
-            },
-            _ => {}
-        }
-    }
-
-    pub fn process_events(&mut self, event: &WindowEvent) -> bool {
-        self.controller.process_events(event)
-    }
-}
-
-pub struct CameraController {
-    pub speed: f32,
-    pub is_up_pressed: bool,
-    pub is_down_pressed: bool,
-    pub is_forward_pressed: bool,
-    pub is_backward_pressed: bool,
-    pub is_left_pressed: bool,
-    pub is_right_pressed: bool,
-}
-
-impl CameraController {
-    pub fn new(speed: f32) -> Self {
-        Self {
-            speed,
-            is_up_pressed: false,
-            is_down_pressed: false,
-            is_forward_pressed: false,
-            is_backward_pressed: false,
-            is_left_pressed: false,
-            is_right_pressed: false,
-        }
-    }
-
-    pub fn process_events(&mut self, event: &WindowEvent) -> bool {
-        match event {
-            WindowEvent::KeyboardInput {
-                input: KeyboardInput {
-                    state,
-                    virtual_keycode: Some(keycode),
-                    ..
-                },
-                ..
-            } => {
-                let is_pressed = *state == ElementState::Pressed;
-                match keycode {
-                    VirtualKeyCode::Space => {
-                        self.is_up_pressed = is_pressed;
-                        true
-                    }
-                    VirtualKeyCode::LShift => {
-                        self.is_down_pressed = is_pressed;
-                        true
-                    }
-                    VirtualKeyCode::Up => {
-                        self.is_forward_pressed = is_pressed;
-                        true
-                    }
-                    VirtualKeyCode::Left => {
-                        self.is_left_pressed = is_pressed;
-                        true
-                    }
-                    VirtualKeyCode::Down => {
-                        self.is_backward_pressed = is_pressed;
-                        true
-                    }
-                    VirtualKeyCode::Right => {
-                        self.is_right_pressed = is_pressed;
-                        true
-                    }
-                    _ => false,
-                }
-            }
-            _ => false,
-        }
+    pub fn new<T: 'static + CameraController>(perspective: CameraPerspective, controller: T) -> Camera {
+        Camera { 
+            perspective, 
+            controller: Box::new(controller)
+        } 
     }
 }
 
@@ -198,8 +27,5 @@ impl Component for Camera {
         unimplemented!()
     }
 }
-
-
-
 
 
